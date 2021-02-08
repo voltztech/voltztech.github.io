d27cf9e9f78c1011c8d404f3271b96bc57ec1603
voltztech <78757972+voltztech@users.noreply.github.com>
2021-02-09 00:48:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,349 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Ankar Courier and Cargo Services</title>
+<?php include 'include/header.php' ?>
+
+  
+
+    <div class="site-blocks-cover overlay" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+          <div class="col-md-8" data-aos="fade-up" data-aos-delay="400">
+            
+
+            <h1 class="text-white font-weight-light mb-5 text-uppercase font-weight-bold">Ankar Courier and Cargo Services</h1>
+            <p><a href="#getstarted" class="btn btn-primary py-3 px-5 text-white">Get Started!</a></p>
+
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    <div class="container">
+      <div class="row align-items-center no-gutters align-items-stretch overlap-section">
+        <div class="col-md-4">
+          <div class="feature-1 pricing h-100 text-center">
+            <div class="icon">
+              <span class="icon-dollar"></span>
+            </div>
+            <h2 class="my-4 heading">Best Prices</h2>
+            <p>The shipping rates for outsourcing courier services can add up quickly, but not here with "Ankar Courier and Cargo Services", Being properly equipped with advanced technology systems and high speed carriers, we deliver express delivery services in the mentioned time frame.</p>
+          </div>
+        </div>
+        <div class="col-md-4" id="getstarted">
+          <div class="free-quote bg-dark h-100">
+            <h2 class="my-4 heading  text-center">Get Free Quote</h2>
+            <form method="post">
+              <div class="form-group">
+                <label for="fq_name">Pick up Location</label>
+                <input type="text" class="form-control btn-block" id="fq_name" name="fq_name" placeholder="Enter Name">
+              </div>
+              <div class="form-group">
+                <label for="fq_name">Delivery Location</label>
+                <input type="text" class="form-control btn-block" id="fq_name" name="fq_name" placeholder="Enter Name">
+              </div>
+              <div class="form-group">
+                <label for="fq_name">Item Type</label>
+                <div class="form-check">
+                  
+                  <label class="form-check-label" for="ItemType">
+                    <input class="form-check-input" type="radio" name="ItemType" id="ItemTypeDoc" value="typedocument" checked>
+                    Documents
+                  </label>
+                </div>
+                <div class="form-check">
+                  
+                  <label class="form-check-label" for="ItemTypeParcel">
+                    <input class="form-check-input" type="radio" name="ItemType" id="ItemTypeParcel" value="typeparcel" >
+                    Parcel
+                  </label>
+                </div>
+                <div class="form-check">
+                  
+                  <label class="form-check-label" for="ItemTypeCargo">
+                    <input class="form-check-input" type="radio" name="ItemType" id="ItemTypeCargo" value="typecargo" >
+                    Cargo
+                  </label>
+                </div>
+              </div>
+            
+              <div class="form-group mb-4">
+                <label for="fq_phone">Phone No.</label>
+                <input type="phone" class="form-control btn-block" id="fq_phone" name="fq_phone" placeholder="Enter Phone Number">
+              </div>
+              <div class="form-group">
+                <input type="submit" class="btn btn-primary text-white py-2 px-4 btn-block" value="Get Free Quote">  
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="feature-3 pricing h-100 text-center">
+            <div class="icon">
+              <span class="icon-phone"></span>
+            </div>
+            <h2 class="my-4 heading">24/7 Support</h2>
+            <p>Partened with more than 80+ courier services all around the globe, we are always here to help you with your deliveries.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="mb-0 text-primary">What We Offer</h2>
+            <p class="color-black-opacity-5"></p>
+          </div>
+        </div>
+        <div class="row align-items-stretch">
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-travel"></span></div>
+              <div>
+                <h3>Air Freight</h3>
+                <p>Want to move express shipments around the globe ? We at Ankar Courier and Cargo service are best at shipment of goods through an air carrier.</p>
+                <!-- <p class="mb-0"><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-sea-ship-with-containers"></span></div>
+              <div>
+                <h3>Ocean Freight</h3>
+                <p>Book ocean freight shipping services easily with Ankar Courier and Cargo service. Our ocean freight shipping services cover the entire container freight transportation process including paperwork, customs, and tracking.</p>
+                <!-- <p class="mb-0"><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-frontal-truck"></span></div>
+              <div>
+                <h3>Ground Shipping</h3>
+                <p>Our Ground shipping is an affordable way to get your products to your customers, especially for domestic deliveries. Freight services usually handle this service - deliveries are made with trucks by road.</p>
+                <!-- <p class="mb-0"><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  
+
+    <div class="site-section block-13">
+      <!-- <div class="container"></div> -->
+
+
+      <div class="owl-carousel nonloop-block-13">
+        <div>
+          <a href="#" class="unit-1 text-center">
+            <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+            <div class="unit-1-text">
+              <h3 class="unit-1-heading">Storage</h3>
+            </div>
+          </a>
+        </div>
+
+        <div>
+          <a href="#" class="unit-1 text-center">
+            <img src="images/img_2.jpg" alt="Image" class="img-fluid">
+            <div class="unit-1-text">
+              <h3 class="unit-1-heading">Air Transports</h3>
+            </div>
+          </a>
+        </div>
+
+        <div>
+          <a href="#" class="unit-1 text-center">
+            <img src="images/img_3.jpg" alt="Image" class="img-fluid">
+            <div class="unit-1-text">
+              <h3 class="unit-1-heading">Cargo Transports</h3>
+            </div>
+          </a>
+        </div>
+
+        <div>
+          <a href="#" class="unit-1 text-center">
+            <img src="images/img_4.jpg" alt="Image" class="img-fluid">
+            <div class="unit-1-text">
+              <h3 class="unit-1-heading">Cargo Ship</h3>
+            </div>
+          </a>
+        </div>
+
+        <div>
+          <a href="#" class="unit-1 text-center">
+            <img src="images/img_5.jpg" alt="Image" class="img-fluid">
+            <div class="unit-1-text">
+              <h3 class="unit-1-heading">Ware Housing</h3>
+            </div>
+          </a>
+        </div>
+
+
+      </div>
+    </div>
+
+
+    
+
+    <div class="site-section bg-light" >
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary" id="services">More Services</h2>
+            <p class="color-black-opacity-5">We Offer The Following Services</p>
+          </div>
+        </div>
+        <div class="row align-items-stretch">
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-travel"></span></div>
+              <div>
+                <h3>Air Air Freight</h3>
+                <p>"Ankar Courier and Cargo Services" along with their partner freight forwarders assisted us in ensuring the delivery of goods was done in a timely and convenient manner for our end customers, helping us with all documentation including the Airway Bill, insurance and the trade finance facility.</p>
+                <!-- <p><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-sea-ship-with-containers"></span></div>
+              <div>
+                <h3>Ocean Freight</h3>
+                <p>Booking an ocean freight has never been esier but with Ankar Courier and Cargo service, Cargo shipping has become easy. Our ocean freight shipping services cover the entire container freight transportation process including paperwork, customs, and tracking.</p>
+                <!-- <p><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-frontal-truck"></span></div>
+              <div>
+                <h3>Ground Shipping</h3>
+                <p>Get guaranteed, date-scheduled, cost-effective ground shipping delivery for commercial & residential shipments. We at ankar Courier and Cargo Services provide you An economical choice for all of your routine shipments with cost-friendly ground shipping rates</p>
+                <!-- <p><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-barn"></span></div>
+              <div>
+                <h3>Warehousing</h3>
+                <p>Looking for warehouse for storing goods that will be sold or distributed later. We at Ankar Courier and Cargo Service, will help you connect with various small, Medium to large warehousing space that is specifically designed to keep your product safe and secure.</p>
+                <!-- <p><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-platform"></span></div>
+              <div>
+                <h3>Storage</h3>
+                <p>Like Warehousing We at Ankar Courier and Cargo Service, will help you connect with various small, Medium to large warehousing space that is specifically designed for storage.</p>
+                <!-- <p><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-4">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary flaticon-car"></span></div>
+              <div>
+                <h3>Delivery Van</h3>
+                <p> Ankar Courier and Cargo van freight delivery services are ideal for when you have a smaller load that requires quick and efficient delivery</p>
+                <!-- <p><a href="#">Learn More</a></p> -->
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+    
+    <div class="site-section border-bottom">
+      <div class="container">
+
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="font-weight-light text-primary">Testimonials</h2>
+          </div>
+        </div>
+
+        <div class="slide-one-item home-slider owl-carousel">
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <img src="images/person_3.jpg" alt="Image" class="img-fluid mb-3">
+                <p>John Smith</p>
+              </figure>
+              <blockquote>
+                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+              </blockquote>
+            </div>
+          </div>
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <img src="images/person_2.jpg" alt="Image" class="img-fluid mb-3">
+                <p>Christine Aguilar</p>
+              </figure>
+              <blockquote>
+                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+              </blockquote>
+            </div>
+          </div>
+
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <img src="images/person_4.jpg" alt="Image" class="img-fluid mb-3">
+                <p>Robert Spears</p>
+              </figure>
+              <blockquote>
+                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+              </blockquote>
+            </div>
+          </div>
+
+          <div>
+            <div class="testimonial">
+              <figure class="mb-4">
+                <img src="images/person_5.jpg" alt="Image" class="img-fluid mb-3">
+                <p>Bruce Rogers</p>
+              </figure>
+              <blockquote>
+                <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+              </blockquote>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    
+    
+    <div class="site-section border-top">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-12">
+            <h2 class="mb-5 text-black">Try Our Services</h2>
+            <p class="mb-0"><a href="/contact.php" class="btn btn-primary py-3 px-5 text-white">Contact Us</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <?php include 'include/footer.php' ?>
